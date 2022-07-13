9b6e68f98043ed00fbad998880b70902993fe50e
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-07-13 17:35:53 +0400

    operators done

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int once = 1;
+        int twice = 10;
+        int third = 100;
+        System.out.print(once);
+        System.out.print(twice);
+        System.out.println(third);
     }
 }
